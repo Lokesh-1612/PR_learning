100b3892a8aacdf08382a4464735f35b88884a30
Udayrao24 <gujjaudayrao@gmail.com>
2026-04-03 14:54:02 +0530

    addition with division

--- Addition.java
+++ Addition.java
@@ -5,4 +5,13 @@ public class Addition {
         int sum = num1 + num2;
         System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
     }
+
+    public class division {
+        public static void main(String[] args) {
+            int num1 = 20;
+            int num2 = 4;
+            int quotient = num1 / num2;
+            System.out.println("The quotient of " + num1 + " and " + num2 + " is: " + quotient);
+        }
+    }
 }
