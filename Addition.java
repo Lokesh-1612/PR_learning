166f3d32adde861815e4edf7a7ccc6887594bec7
Lokesh-1612 <lokeshnayak1612@gmail.com>
2026-04-03 15:02:32 +0530

    Adddition third modifed

---
+++ Addition.java
@@ -0,0 +1,17 @@
+public class Addition {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+
+    public class division {
+        public static void main(String[] args) {
+            int num1 = 20;
+            int num2 = 4;
+            int quotient = num1 / num2;
+            System.out.println("The quotient of  " + num1 + " and " + num2 + " is: " + quotient);
+        }
+    }
+}
